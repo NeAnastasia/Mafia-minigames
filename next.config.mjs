db344583eb495af190d0b8e47381a4793da80859
NeAnastasia <neanastasi@mail.ru>
2025-07-15 22:20:37 +0700

    trying

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', // Статический экспорт для GitHub Pages
-  basePath: process.env.NODE_ENV === 'production' ? '/Mafia-minigames' : '', // Указывает имя репозитория
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Mafia-minigames/' : '', // Для корректных путей к CSS/JS/изображениям
   trailingSlash: true, // Добавляет `/` в конце URL (рекомендуется для GitHub Pages)
   images: {
     unoptimized: true, // Отключает оптимизацию изображений (иначе могут быть проблемы)
